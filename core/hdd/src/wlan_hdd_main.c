90c7f90c5dd14d06232eb4cb664b600740160182
xiangming1 <xiangming1@lenovo.com>
2020-09-11 17:47:50 +0800

    Fix QC release issue at Viking
    Some unused code will result in compilation error after this release
    at kernel 5.4, so remove them to fix compilation issue
    
    Change-Id: I0a71cc9555e1e2e937024faa7aebb32a033f74d4
    Signed-off-by: xiangming1 <xiangming1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1747068
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ningkai Zhuang <zhuangnk1@mt.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -1103,7 +1103,8 @@ EXPORT_SYMBOL(con_mode_ftm);
 #endif
 int con_mode_epping;
 
-static int pcie_gen_speed;
+//IKSWR-4630 Removed unused code because it will result in compilation error at Kernel 5.4
+//static int pcie_gen_speed;
 
 /* Variable to hold connection mode including module parameter con_mode */
 static int curr_con_mode;
@@ -18941,6 +18942,8 @@ static const struct kernel_param_ops fwpath_ops = {
 	.get = param_get_string,
 };
 
+//IKSWR-4630 Removed unused code because it will result in compilation error at Kernel 5.4
+/*
 static int __pcie_set_gen_speed_handler(void)
 {
 	int ret;
@@ -18985,6 +18988,7 @@ out:
 
 	return ret;
 }
+*/
 
 static const struct kernel_param_ops sar_ops = {
 	.set = sar_changed_handler,
@@ -18997,8 +19001,11 @@ module_param_cb(con_mode, &con_mode_ops, &con_mode,
 module_param_cb(con_mode_ftm, &con_mode_ftm_ops, &con_mode_ftm,
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
+//IKSWR-4630 Removed unused code because it will result in compilation error at Kernel 5.4
+/*
 module_param_cb(pcie_gen_speed, &pcie_gen_speed_ops, &pcie_gen_speed,
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+*/
 
 #ifdef WLAN_FEATURE_EPPING
 module_param_cb(con_mode_epping, &con_mode_epping_ops,
