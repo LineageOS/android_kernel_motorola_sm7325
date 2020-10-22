1d496253dfb43fd60c06632d05011161441310d4
houdz1 <houdz1@motorola.com>
2020-10-22 15:21:46 +0800

    disp: msm: reset the param value to the default value
    This code is pick from the project with Android-p. But there is a code
    error when make propagation from the previous project.
    We must reset the param after panel enabled every time.
    
    Mot-CRs-fixed: (CR)
    
    Signed-off-by: houdz1 <houdz1@motorola.com>
    Change-Id: Iea6a0b6152cab0e73040aa274c632439d9b4cebb
    Reviewed-on: https://gerrit.mot.com/1783909
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_display.c
+++ msm/dsi/dsi_display.c
@@ -8999,7 +8999,7 @@ int dsi_display_enable(struct dsi_display *display)
 			       display->name, rc);
 			goto error;
 		}
-
+		dsi_panel_reset_param(display->panel);
 		dsi_display_enable_status(display, true);
 	}
 	dsi_display_panel_id_notification(display);
@@ -9013,7 +9013,6 @@ int dsi_display_enable(struct dsi_display *display)
 				display->name, rc);
 			goto error;
 		}
-		dsi_panel_reset_param(display->panel);
 	}
 
 	if (mode->dsi_mode_flags & DSI_MODE_FLAG_DMS) {
