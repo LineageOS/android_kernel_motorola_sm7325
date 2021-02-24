ebdbb079d62f479a94beb5d0eb5e4f6ec29ab3cd
huangys <huangys@motorola.com>
2021-02-25 01:43:00 +0800

    asoc: Berlna audio bringup
    It use QC wcd937x audio codec and Awinic aw882xx boost chip for speaker
    output. ,and It doesn't have 3.5 headphone switch, so we need suport usbc
    analog headset interface.
    
    It has 3 analog microphone connecting to codec.
    
    Change-Id: I774910a910c0bf2d12eaa79e77284c3be2cfc66e
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1883312
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Dandan Du <dudd1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- asoc/lahaina.c
+++ asoc/lahaina.c
@@ -6907,6 +6907,32 @@ static struct snd_soc_dai_link msm_mi2s_reed_dai_links[] = {
 	},
 };
 
+static struct snd_soc_dai_link msm_mi2s_aw882xx_dai_links[] = {
+	{
+		.name = LPASS_BE_SENARY_MI2S_RX,
+		.stream_name = "Senary MI2S Playback",
+		.no_pcm = 1,
+		.dpcm_playback = 1,
+		.id = MSM_BACKEND_DAI_SENARY_MI2S_RX,
+		.be_hw_params_fixup = msm_be_hw_params_fixup,
+		.ops = &msm_mi2s_be_ops,
+		.ignore_suspend = 1,
+		.ignore_pmdown_time = 1,
+		SND_SOC_DAILINK_REG(sen_mi2s_rx_aw882xx),
+	},
+	{
+		.name = LPASS_BE_SENARY_MI2S_TX,
+		.stream_name = "Senary MI2S Capture",
+		.no_pcm = 1,
+		.dpcm_capture = 1,
+		.id = MSM_BACKEND_DAI_SENARY_MI2S_TX,
+		.be_hw_params_fixup = msm_be_hw_params_fixup,
+		.ops = &msm_mi2s_be_ops,
+		.ignore_suspend = 1,
+		SND_SOC_DAILINK_REG(sen_mi2s_tx_aw882xx),
+	},
+};
+
 static struct snd_soc_dai_link msm_mi2s_be_dai_links[] = {
 	{
 		.name = LPASS_BE_PRI_MI2S_RX,
@@ -7742,6 +7768,7 @@ static struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 	u32 wsa_max_devs = 0;
 	int cirrus_prince_max_devs = 0;
 	int cirrus_franklin_max_devs = 0;
+	int awinic_aw882xx_max_devs = 0;
 
 	match = of_match_node(lahaina_asoc_machine_of_match, dev->of_node);
 	if (!match) {
@@ -7824,10 +7851,15 @@ static struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 			if (rc)
 				cirrus_franklin_max_devs = 0;
 
-			dev_info(dev,
-				"%s: prince-max-devs %d franklin-max-devs %d\n",
-				 __func__, cirrus_prince_max_devs, cirrus_franklin_max_devs);
+			rc = of_property_read_u32(dev->of_node,
+					"awinic,aw882xx-max-devs", &awinic_aw882xx_max_devs);
+			if (rc)
+				awinic_aw882xx_max_devs = 0;
 
+			dev_info(dev,
+				"%s: prince-max-devs %d franklin-max-devs %d awinic_aw882xx_max_devs %d\n",
+				 __func__, cirrus_prince_max_devs, cirrus_franklin_max_devs,
+				 awinic_aw882xx_max_devs);
 
 			if (cirrus_prince_max_devs == 2) {
 				memcpy(msm_lahaina_dai_links + total_links,
@@ -7847,6 +7879,12 @@ static struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 					sizeof(msm_mi2s_franklin_mono_dai_links));
 				total_links +=
 					ARRAY_SIZE(msm_mi2s_franklin_mono_dai_links);
+			} else if (awinic_aw882xx_max_devs == 1) {
+				memcpy(msm_lahaina_dai_links + total_links,
+					msm_mi2s_aw882xx_dai_links,
+					sizeof(msm_mi2s_aw882xx_dai_links));
+				total_links +=
+					ARRAY_SIZE(msm_mi2s_aw882xx_dai_links);
 			} else if (mi2s_audio_intf) {
 				memcpy(msm_lahaina_dai_links + total_links,
 					msm_mi2s_be_dai_links,
@@ -7874,8 +7912,6 @@ static struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 				total_links +=
 					ARRAY_SIZE(msm_mi2s_reed_dai_links);
 			}
-
-
 		}
 #ifndef CONFIG_AUXPCM_DISABLE
 		rc = of_property_read_u32(dev->of_node,
--- asoc/msm_dailink.h
+++ asoc/msm_dailink.h
@@ -1024,6 +1024,16 @@ SND_SOC_DAILINK_DEFS(sen_mi2s_tx,
 	DAILINK_COMP_ARRAY(COMP_CODEC("msm-stub-codec.1", "msm-stub-tx")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("msm-pcm-routing")));
 
+SND_SOC_DAILINK_DEFS(sen_mi2s_rx_aw882xx,
+	DAILINK_COMP_ARRAY(COMP_CPU("msm-dai-q6-mi2s.5")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("aw882xx_codec", "aw882xx-aif")),
+	DAILINK_COMP_ARRAY(COMP_PLATFORM("msm-pcm-routing")));
+
+SND_SOC_DAILINK_DEFS(sen_mi2s_tx_aw882xx,
+	DAILINK_COMP_ARRAY(COMP_CPU("msm-dai-q6-mi2s.5")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("aw882xx_codec", "aw882xx-aif")),
+	DAILINK_COMP_ARRAY(COMP_PLATFORM("msm-pcm-routing")));
+
 SND_SOC_DAILINK_DEFS(auxpcm_rx,
 	DAILINK_COMP_ARRAY(COMP_CPU("msm-dai-q6-auxpcm.1")),
 	DAILINK_COMP_ARRAY(COMP_CODEC("msm-stub-codec.1", "msm-stub-rx")),
