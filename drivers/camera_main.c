b62483c656259292f192dccc070446f8919dc6f2
wangshuo18 <wangshuo18@lenovo.com>
2021-04-07 15:47:20 +0800

    cam: add macro CONFIG_AF_NOISE_ELIMINATION judgment
    -fix build error
    -only integrate mot_actuator driver on device which enabled
    CONFIG_AF_NOISE_ELIMINATION
    -modify dac_pos to 480,follow hanoip
    
    Change-Id: I7205cd01df239ede3c7a54609d4c8eb54f42eb87
    Signed-off-by: wangshuo18 <wangshuo18@mt.com>
    Reviewed-on: https://gerrit.mot.com/1918702
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shuo Wang <wangsh18@motorola.com>
    Reviewed-by: Dawei Wang <wangdw10@motorola.com>
    Reviewed-by: Bolei Shang <shangbl1@mt.com>
    Reviewed-by: Ji Zhao <zhaoji1@mt.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/camera_main.c
+++ drivers/camera_main.c
@@ -20,7 +20,9 @@
 #include "cam_cci_dev.h"
 #include "cam_sensor_dev.h"
 #include "cam_actuator_dev.h"
+#ifdef CONFIG_AF_NOISE_ELIMINATION
 #include "mot_actuator.h"
+#endif
 #include "cam_csiphy_dev.h"
 #include "cam_eeprom_dev.h"
 #include "cam_ois_dev.h"
@@ -106,7 +108,9 @@ static const struct camera_submodule_component camera_sensor[] = {
 	{&cam_cci_init_module, &cam_cci_exit_module},
 	{&cam_csiphy_init_module, &cam_csiphy_exit_module},
 	{&cam_actuator_driver_init, &cam_actuator_driver_exit},
+#ifdef CONFIG_AF_NOISE_ELIMINATION
 	{&mot_actuator_driver_init, &mot_actuator_driver_exit},
+#endif
 	{&cam_sensor_driver_init, &cam_sensor_driver_exit},
 	{&cam_eeprom_driver_init, &cam_eeprom_driver_exit},
 	{&cam_ois_driver_init, &cam_ois_driver_exit},
