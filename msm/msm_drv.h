5f13d47ef440ac589318a502565d41e977d2605c
daiss1 <daiss1@lenovo.com>
2021-05-10 11:14:30 +0800

    drm: Add the DC panel param feature
    Add the DC panel param feature which will be controlled
    from the user-space
    
    Change-Id: Ia241893614f91ba209ce830db033033235da2dfc
    Signed-off-by: daiss1 <daiss1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1943795
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/msm_drv.h
+++ msm/msm_drv.h
@@ -219,6 +219,7 @@ enum msm_mdp_conn_property {
 	CONNECTOR_PROP_HBM,
 	CONNECTOR_PROP_CABC,
 	CONNECTOR_PROP_ACL,
+	CONNECTOR_PROP_DC,
 	/* total # of properties */
 	CONNECTOR_PROP_COUNT
 };
@@ -367,6 +368,7 @@ enum msm_param_id {
 	PARAM_HBM_ID = 0,
 	PARAM_CABC_ID,
 	PARAM_ACL_ID,
+	PARAM_DC_ID,
 	PARAM_ID_NUM
 };
 
