8bfaa36523c9a6442c96275392bd668cb0930634
liuwang2 <liuwang2@lenovo.com>
2021-12-08 09:37:59 +0800

    camera: ois: support vsync interrupt
    [detail]
    Use vsync interrupt timestamp for ois data timestamp.
    
    Change-Id: I56ffced64ad368129115067f84109d8a66663f59
    Signed-off-by: liuwang2 <liuwang2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2138044
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_ois/cam_ois_soc.c
+++ drivers/cam_sensor_module/cam_ois/cam_ois_soc.c
@@ -34,6 +34,14 @@ static int cam_ois_get_dt_data(struct cam_ois_ctrl_t *o_ctrl)
 			o_ctrl->ois_device_type);
 		return -EINVAL;
 	}
+
+	if (!of_property_read_bool(of_node, "vsync-irq-support")) {
+		o_ctrl->is_ois_vsync_irq_supported = false;
+	} else {
+		o_ctrl->is_ois_vsync_irq_supported = true;
+	}
+	CAM_DBG(CAM_OIS, "is_ois_vsync_irq_supported %d", o_ctrl->is_ois_vsync_irq_supported);
+
 	rc = cam_soc_util_get_dt_properties(soc_info);
 	if (rc < 0) {
 		CAM_ERR(CAM_OIS, "cam_soc_util_get_dt_properties rc %d",
