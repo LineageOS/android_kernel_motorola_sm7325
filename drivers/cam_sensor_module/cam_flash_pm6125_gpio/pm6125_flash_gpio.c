9b2d3e8eb78fc82a3c8abafeaf747290c1324af2
huaqin78 <huaqin78@motorola.com>
2021-12-11 16:24:16 +0800

    Rhodep: add flash current ma control
    details:
    add flash current ma control
    
    Change-Id: Ic3c89657ac568cf56fc2f402c90211fb433b6212
    Signed-off-by: huaqin78 <huaqin78@motorola.com>
    Signed-off-by: luozhq15 <luozhq15@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2142725
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: <huaqin90@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_flash_pm6125_gpio/pm6125_flash_gpio.c
+++ drivers/cam_sensor_module/cam_flash_pm6125_gpio/pm6125_flash_gpio.c
@@ -35,28 +35,55 @@ struct pwm_device *pwm;
  * Function
  *****************************************************************************/
 
-void pm6125_flash_gpio_select_state(PM6125_FLASH_GPIO_STATE s, u64 flash_current){
-    struct pwm_state pstate;
-    int rc = 0;
-    pwm_get_state(pwm, &pstate);
-    pstate.period = 50000;
-    pstate.duty_cycle = 500000*flash_current/10000;
-    switch(s){
-    case PM6125_FLASH_GPIO_STATE_ACTIVE:
-        pstate.enabled = true;
-        rc = pwm_apply_state(pwm, &pstate);
-        break;
-    case PM6125_FLASH_GPIO_STATE_SUSPEND:
-        pstate.enabled = false;
-        rc = pwm_apply_state(pwm, &pstate);
-        break;
-    default:
-        PM6125_FLASH_PRINT("[pm6125_flash_gpio]Failed to control PWM use a err state!\n");
-    }
+void pm6125_flash_gpio_select_state(PM6125_FLASH_GPIO_STATE s, enum camera_flash_opcode opcode, u64 flash_current){
+	struct pwm_state pstate;
+	int rc = 0;
+	u64 real_current = 0;
 
-    if(rc < 0) {
-        PM6125_FLASH_PRINT("[pm6125_flash_gpio]Apply PWM state fail, rc = %d", rc);
-    }
+	pwm_get_state(pwm, &pstate);
+	PM6125_FLASH_PRINT("[pm6125_flash_gpio]Status duty_cycle = %u, period = %u\n",
+					pstate.duty_cycle, pstate.period);
+	pstate.period = PM6125_PWM_PERIOD;
+	switch (s) {
+	case PM6125_FLASH_GPIO_STATE_ACTIVE:
+		pstate.duty_cycle = 0;
+		pstate.enabled = false;
+		rc = pwm_apply_state(pwm, &pstate);
+		PM6125_FLASH_PRINT("[pm6125_flash_gpio]Suspend before Active");
+
+		switch (opcode)
+		{
+		case CAMERA_SENSOR_FLASH_OP_FIRELOW:
+			real_current = (flash_current > FLASH_FIRE_LOW_MAXCURRENT?FLASH_FIRE_LOW_MAXCURRENT:flash_current);
+			pstate.duty_cycle = PM6125_PWM_PERIOD * real_current / FLASH_FIRE_LOW_MAXCURRENT;
+			break;
+		case CAMERA_SENSOR_FLASH_OP_FIREHIGH:
+			real_current = (flash_current > FLASH_FIRE_HIGH_MAXCURRENT?FLASH_FIRE_HIGH_MAXCURRENT:flash_current);
+			pstate.duty_cycle = PM6125_PWM_PERIOD * real_current / FLASH_FIRE_HIGH_MAXCURRENT;
+			break;
+		default:
+			pstate.duty_cycle = PM6125_PWM_PERIOD;
+			break;
+		}
+
+		pstate.enabled = true;
+		rc = pwm_apply_state(pwm, &pstate);
+
+		PM6125_FLASH_PRINT("[pm6125_flash_gpio]Active duty_cycle = %u, period = %u, opcode = %u, real_current = %u\n",
+						pstate.duty_cycle, pstate.period, opcode, real_current);
+		break;
+	case PM6125_FLASH_GPIO_STATE_SUSPEND:
+		pstate.enabled = false;
+		rc = pwm_apply_state(pwm, &pstate);
+		PM6125_FLASH_PRINT("[pm6125_flash_gpio]Suspend");
+		break;
+	default:
+		PM6125_FLASH_PRINT("[pm6125_flash_gpio]Failed to control PWM use a err state!\n");
+	}
+
+	if(rc < 0) {
+		PM6125_FLASH_PRINT("[pm6125_flash_gpio]Apply PWM state fail, rc = %d", rc);
+	}
 }
 
 static int pm6125_flash_pwm_probe(struct platform_device *pdev)
@@ -69,30 +96,30 @@ static int pm6125_flash_pwm_probe(struct platform_device *pdev)
 }
 
 static const struct of_device_id gpio_of_match[] = {
-    { .compatible = "qualcomm,pm6125_flash_gpio", },
-    {},
+	{ .compatible = "qualcomm,pm6125_flash_gpio", },
+	{},
 };
 
 static struct platform_driver pm6125_flash_gpio_platform_driver = {
-    .probe = pm6125_flash_pwm_probe,
-    .driver = {
-        .name = "PM6125_FLASH_GPIO_DTS",
-        .of_match_table = gpio_of_match,
-    },
+	.probe = pm6125_flash_pwm_probe,
+	.driver = {
+		.name = "PM6125_FLASH_GPIO_DTS",
+		.of_match_table = gpio_of_match,
+	},
 };
 
 int pm6125_flash_gpio_init_module(void)
 {
-    if (platform_driver_register(&pm6125_flash_gpio_platform_driver)) {
-	    PM6125_FLASH_PRINT("[pm6125_flash_gpio]Failed to register pm6125_flash_gpio_platform_driver!\n");
-	    return -1;
-    }
-    return 0;
+	if (platform_driver_register(&pm6125_flash_gpio_platform_driver)) {
+		PM6125_FLASH_PRINT("[pm6125_flash_gpio]Failed to register pm6125_flash_gpio_platform_driver!\n");
+		return -1;
+	}
+	return 0;
 }
 
 void pm6125_flash_gpio_exit_module(void)
 {
-    platform_driver_unregister(&pm6125_flash_gpio_platform_driver);
+	platform_driver_unregister(&pm6125_flash_gpio_platform_driver);
 }
 
 MODULE_AUTHOR("liyang <liyang14@huaqin.com>");
