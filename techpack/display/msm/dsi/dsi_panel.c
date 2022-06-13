06ce2899398012eceed2da68785f9d2aa23f5091
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-13 09:40:07 +0300

    techpack: display: dsi_panel: cleanup FOD HBM path
    Change-Id: I0a374998f47a48c5fe91b10f5fcc3c7c4d99e40d

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -989,10 +989,6 @@ static int dsi_panel_send_param_cmd(struct dsi_panel *panel,
 
         param_map = panel_param->val_map;
 
-	DSI_INFO("%s: param_name=%s; val_max =%d, default_value=%d, value=%d\n",
-	        __func__, panel_param->param_name, panel_param->val_max,
-		panel_param->default_value, panel_param->value);
-
 	mutex_lock(&panel->panel_lock);
 
 	if (!panel->panel_initialized) {
@@ -1005,12 +1001,8 @@ static int dsi_panel_send_param_cmd(struct dsi_panel *panel,
 
 	if (panel_param->value == param_info->value && param_info->param_idx != PARAM_DC_ID)
 	{
-		DSI_INFO("(mode=%d): requested value=%d is same. Do nothing\n",
-			param_info->param_idx, param_info->value);
 		rc = 0;
 	} else {
-		DSI_DEBUG("%s: requested: old=%d new=%d.\n", __func__,
-			panel_param->value, param_info->value);
 		param_map = panel->param_cmds[param_info->param_idx].val_map;
 		param_map_state = &param_map[param_info->value];
 
@@ -1038,8 +1030,6 @@ static int dsi_panel_send_param_cmd(struct dsi_panel *panel,
 			cmds++;
 		}
 		panel_param->value = param_info->value;
-		DSI_INFO("(%d) is setting new value %d\n",
-			param_info->param_idx, param_info->value);
 		rc = len;
 	}
 
@@ -1131,36 +1121,7 @@ static int dsi_panel_set_hbm(struct dsi_panel *panel,
 	int rc = 0;
 	u32 bl_lvl;
 	struct dsi_panel_lhbm_config *lhbm_config = &panel->lhbm_config;
-	char name[30], val[30];
-	char *envp[3];
-	struct panel_param *panel_param;
 
-	snprintf(name, 30, "name=%s", "HBM");
-	snprintf(val, 30, "status=%d", param_info->value);
-	pr_info("[%s] [%s]\n", name, val);
-
-	if(lhbm_config->enable && param_info->value != HBM_ON_STATE) {
-		dsi_panel_set_local_hbm_param(panel, param_info, lhbm_config);
-	}
-
-	panel_param = &panel->param_cmds[param_info->param_idx];
-	if (!panel_param) {
-		DSI_ERR("%s: invalid panel_param.\n", __func__);
-		return -EINVAL;
-	}
-	if (panel_param->value == param_info->value)
-	{
-		DSI_INFO("(mode=%d): requested value=%d is same. Do nothing\n",
-				param_info->param_idx, param_info->value);
-		return 0;
-	}
-
-	envp[0] = name;
-	envp[1] = val;
-	envp[2] = NULL;
-	kobject_uevent_env(&panel->parent->kobj, KOBJ_CHANGE, envp);
-
-	pr_info("Set HBM to (%d)\n", param_info->value);
 	rc = dsi_panel_send_param_cmd(panel, param_info);
 	if (rc < 0) {
 		DSI_ERR("%s: failed to send param cmds. ret=%d\n", __func__, rc);
