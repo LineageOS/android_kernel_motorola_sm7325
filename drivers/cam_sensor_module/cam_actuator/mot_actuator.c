31245df6cbd7c26afa6f33cbd29fbb2784b60215
HQ Cam LiJunyu <lijunyu@huaqin.com>
2022-09-27 14:54:49 +0800

    penang: Adjust actuator noise reduction
    details:
    Add drive node
    Modify the motor voltage value
    
    Change-Id: I8abcd884a33e4326ea1066a1ca810e7e23ba3e67
    Signed-off-by: HQ Cam LiJunyu <lijunyu@huaqin.com>
    Reviewed-on: https://gerrit.mot.com/2392270
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <chenlj26@motorola.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Shuo Wang <wangsh18@motorola.com>
    Reviewed-by: Wang Liu <liuwang2@mt.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_actuator/mot_actuator.c
+++ drivers/cam_sensor_module/cam_actuator/mot_actuator.c
@@ -44,6 +44,7 @@ typedef enum {
 	MOT_ACTUATOR_GT9772,
 	MOT_ACTUATOR_DW9714,
 	MOT_ACTUATOR_DW9800V,
+	MOT_ACTUATOR_PENANG_DW9800V,
 	MOT_ACTUATOR_NUM,
 } mot_actuator_type;
 
@@ -221,6 +222,21 @@ static struct cam_sensor_i2c_reg_setting mot_dw9800v_dac_settings = {
 	.data_type = CAMERA_SENSOR_I2C_TYPE_WORD,
 };
 
+/*Register settings of Device of Penang DW9800V*/
+static struct cam_sensor_i2c_reg_array mot_penang_dw9800v_init_setting[] ={
+	{0x02, 0x02, 0},
+	{0x06, 0x80, 0},
+	{0x07, 0x62, 0},
+	{0x03, 0x00, 0},
+};
+
+static struct cam_sensor_i2c_reg_setting mot_penang_dw9800v_init_settings = {
+	.reg_setting = mot_penang_dw9800v_init_setting,
+	.size = ARRAY_SIZE(mot_penang_dw9800v_init_setting),
+	.addr_type = CAMERA_SENSOR_I2C_TYPE_BYTE,
+	.data_type = CAMERA_SENSOR_I2C_TYPE_BYTE,
+};
+
 /*Register settings of all supported actuator types*/
 static mot_actuator_settings mot_actuator_list[MOT_ACTUATOR_NUM-1] = {
 	//MUST be sorted as definition order in above structure: mot_actuator_type
@@ -228,6 +244,7 @@ static mot_actuator_settings mot_actuator_list[MOT_ACTUATOR_NUM-1] = {
 	{&mot_gt9772_init_settings, &mot_gt9772_dac_settings},
 	{&mot_dw9714_init_settings, &mot_dw9714_dac_settings},
 	{&mot_dw9800v_init_settings, &mot_dw9800v_dac_settings},
+	{&mot_penang_dw9800v_init_settings},
 };
 
 static const mot_dev_info mot_dev_list[MOT_DEVICE_NUM] = {
@@ -506,7 +523,7 @@ static const mot_dev_info mot_dev_list[MOT_DEVICE_NUM] = {
 		.dev_name = "penang",
 		.actuator_info = {
 			[0] = {
-				.actuator_type = MOT_ACTUATOR_DW9800V,
+				.actuator_type = MOT_ACTUATOR_PENANG_DW9800V,
 				.dac_pos = 0,
 				.init_pos = 512,
 				.cci_addr = 0x0c,
@@ -514,14 +531,6 @@ static const mot_dev_info mot_dev_list[MOT_DEVICE_NUM] = {
 				.cci_master = 0x1,
 				.regulator_list = {"camera_ldo_dovdd", "pm6125_l21"},
 				.regulator_volt_uv = {1800000, 2800000},
-				.launch_lens = {
-						.launch_lens_needed = true,
-						.launch_lens_step = {
-									{200, 100},
-									{100, 60},
-									{50, 30},
-						},
-				},
 			},
 		},
 	},
@@ -951,13 +960,21 @@ static int32_t mot_actuator_vib_move_lens(uint32_t index)
 		if (consumers == 0) {
 			/*Just move lens when camera off and before first vibrating*/
 			/*use launch_lens to move lens step-by-step,reduce TICK noise*/
-			if (mot_dev_list[mot_device_index].actuator_info[index].launch_lens.launch_lens_needed == true) {
-				ret = mot_actuator_launch_lens(index, mot_actuator_runtime[index].safe_dac_pos);
-			}else {
-				ret = mot_actuator_move_lens_by_dac(index, mot_actuator_runtime[index].safe_dac_pos);
-				/*delay 10~12ms to wait lens move to specify location*/
-				usleep_range(10000, 12000);
+			if(mot_device_index == MOT_DEVICE_PENANG)
+			{
+				ret = 0;
+				CAM_WARN(CAM_ACTUATOR, "Device is Penang 5G,skip move lens !!!!");
 			}
+			else {
+				if (mot_dev_list[mot_device_index].actuator_info[index].launch_lens.launch_lens_needed == true) {
+					ret = mot_actuator_launch_lens(index, mot_actuator_runtime[index].safe_dac_pos);
+				}else {
+					ret = mot_actuator_move_lens_by_dac(index, mot_actuator_runtime[index].safe_dac_pos);
+					/*delay 10~12ms to wait lens move to specify location*/
+					usleep_range(10000, 12000);
+				}
+			}
+
 			if (ret == 0) {
 				CAM_WARN(CAM_ACTUATOR, "actuator is safe now, safe_dac_pos:%d, please start vibrating.",
 					mot_actuator_runtime[index].safe_dac_pos);
