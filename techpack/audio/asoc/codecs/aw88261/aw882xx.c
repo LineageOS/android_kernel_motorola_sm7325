86c3f33e1b6a8afe5ab99bb2ef4410a2b82492c5
Cosmin Tanislav <demonsingur@gmail.com>
2022-10-03 10:04:21 +0300

    techpack: audio: aw88261: solve unused variable error
    techpack/audio/asoc/codecs/aw88261/aw882xx.c:1289:20:
    error: variable 'aw_dev' set but not used [-Werror,-Wunused-but-set-variable]
            struct aw_device *aw_dev;
    
    Change-Id: I9a3acd6070f8b7e10afce76e82768ab78262dc27

--- techpack/audio/asoc/codecs/aw88261/aw882xx.c
+++ techpack/audio/asoc/codecs/aw88261/aw882xx.c
@@ -1329,7 +1329,6 @@ static int aw882xx_set_spin(struct snd_kcontrol *kcontrol,
 {
 	int ret = -EINVAL;
 	uint32_t ctrl_value = 0;
-	struct aw_device *aw_dev;
 	aw_snd_soc_codec_t *codec =
 		aw_componet_codec_ops.kcontrol_codec(kcontrol);
 	struct aw882xx *aw882xx =
@@ -1337,8 +1336,6 @@ static int aw882xx_set_spin(struct snd_kcontrol *kcontrol,
 
 	aw_dev_dbg(aw882xx->dev, "ucontrol->value.integer.value[0]=%ld",
 			ucontrol->value.integer.value[0]);
-
-	aw_dev = aw882xx->aw_pa;
 
 	ctrl_value = ucontrol->value.integer.value[0];
 	if (aw882xx->pstream) {
@@ -1356,15 +1353,12 @@ static int aw882xx_set_spin(struct snd_kcontrol *kcontrol,
 static int aw882xx_get_spin(struct snd_kcontrol *kcontrol,
 	struct snd_ctl_elem_value *ucontrol)
 {
-	struct aw_device *aw_dev;
 	aw_snd_soc_codec_t *codec =
 		aw_componet_codec_ops.kcontrol_codec(kcontrol);
 	struct aw882xx *aw882xx =
 		aw_componet_codec_ops.codec_get_drvdata(codec);
 	int ctrl_value;
 	int ret = -EINVAL;
-
-	aw_dev = aw882xx->aw_pa;
 
 	if (aw882xx->pstream) {
 		ret = aw_dev_get_spin(&ctrl_value);
