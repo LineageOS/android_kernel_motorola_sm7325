f6b8ee52910d8780ba929cafed65852126b30c7c
Cosmin Tanislav <demonsingur@gmail.com>
2022-12-17 18:18:20 +0200

    Revert "techpack: display: expose forcing FOD UI for debugging"
    This reverts commit f16affd7258074157451990ca8b2656486f9643b.
    
    Change-Id: Ie1c831a7b36b3b00abbe7c05d0ab23207e064776

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -407,7 +407,6 @@ struct dsi_panel {
 	u8 fod_dim_alpha;
 	bool fod_hbm_enabled;
 	bool fod_ui;
-	bool force_fod_ui;
 };
 
 bool dsi_display_all_displays_dead(void);
@@ -564,6 +563,5 @@ int dsi_panel_parse_elvss_config(struct dsi_panel *panel, u8 elv_vl);
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 bool dsi_panel_get_fod_ui(struct dsi_panel *panel);
 void dsi_panel_set_fod_ui(struct dsi_panel *panel, bool status);
-bool dsi_panel_get_force_fod_ui(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
