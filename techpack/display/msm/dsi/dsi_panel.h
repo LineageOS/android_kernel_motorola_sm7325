ce202f55fdbbc8a776fdd02366d2ede1085cdf58
Cosmin Tanislav <demonsingur@gmail.com>
2022-12-19 16:33:16 +0200

    techpack: display: dsi_panel: expose FOD dim alpha
    Change-Id: I2c4431105df6082fcfbaacdbab9165f89720824b

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -563,5 +563,6 @@ int dsi_panel_parse_elvss_config(struct dsi_panel *panel, u8 elv_vl);
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 bool dsi_panel_get_fod_ui(struct dsi_panel *panel);
 void dsi_panel_set_fod_ui(struct dsi_panel *panel, bool status);
+u8 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
