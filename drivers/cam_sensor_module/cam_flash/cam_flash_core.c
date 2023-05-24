63d2e1c17bbb06c065e08fa2f00cf7f260454ca0
tin_zhiyuan.liao_tmp <tinno74@motorola.com>
2023-05-24 14:37:37 +0800

    bangkk: Fix flash request GPIO-24 failed
    details:
      fixed GPIO-24 set shared failed.
    
    Change-Id: Ib7fbec1be357585eb14c4d898c06b80af37bd6ba
    Signed-off-by: tin_zhiyuan.liao_tmp <tinno74@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2619027
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: <yanglp10@motorola.com>
    Reviewed-by: <leijing1@motorola.com>
    Reviewed-by: <chenzm8@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_flash/cam_flash_core.c
+++ drivers/cam_sensor_module/cam_flash/cam_flash_core.c
@@ -2148,7 +2148,7 @@ static int cam_flash_construct_power_vio_setting(
 	power_info->power_setting[0].seq_val = CAM_VIO;
 	power_info->power_setting[0].config_val = 1;
 	power_info->power_setting[0].delay = 2;
-	power_info->power_setting[1].seq_type = SENSOR_CUSTOM_GPIO1;
+	power_info->power_setting[1].seq_type = SENSOR_CUSTOM_REG2;
 	power_info->power_setting[1].seq_val = 0;
 	power_info->power_setting[1].config_val = 1;
 	power_info->power_setting[1].delay = 0;
@@ -2162,7 +2162,7 @@ static int cam_flash_construct_power_vio_setting(
 		goto free_power_settings;
 	}
 
-	power_info->power_down_setting[0].seq_type = SENSOR_CUSTOM_GPIO1;
+	power_info->power_down_setting[0].seq_type = SENSOR_CUSTOM_REG2;
 	power_info->power_down_setting[0].seq_val = 0;
 	power_info->power_down_setting[0].config_val = 0;
 	power_info->power_down_setting[1].seq_type = SENSOR_VIO;
