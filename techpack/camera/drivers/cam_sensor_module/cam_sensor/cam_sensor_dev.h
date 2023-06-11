78d5a39806e52641f25a02b525adb07f2d28a85d
electimon <electimon@gmail.com>
2023-06-11 17:18:56 -0400

    techpack: camera: Add guarding for header struct changes
    * We don't build the Camera HAL from source
      so userspace does not need access to
      cam_sensor.h, move it and implement guarding
      for breaking changes for devices with older
      Camera Blobs.
    
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I3e05d82bfad91f6da3958e798d1e4c5b84cac8cf

--- techpack/camera/drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.h
+++ techpack/camera/drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.h
@@ -117,10 +117,13 @@ struct cam_sensor_ctrl_t {
 	uint8_t   sub_device_addr_type;
 	uint16_t  sub_device_id_addr;
 	uint16_t  expected_sub_device_id;
+#ifdef CONFIG_CAMERA_CCI_MASTER_CHANGE
 	uint8_t   sub_device_cci_master;
 	uint8_t   sub_device_cci_device;
 	uint8_t   sub_device_i2c_freq_mode;
 	bool need_change_cci_master;
+#endif
+#ifdef CONFIG_CAMERA_CCI_ADDR_SWITCH
 	uint8_t i2c_addr_switch;
 	uint16_t second_i2c_address;
 	uint8_t i2c_switch_reg_addr_Type;
@@ -128,6 +131,7 @@ struct cam_sensor_ctrl_t {
 	uint16_t i2c_switch_reg_addr;
 	uint16_t i2c_switch_reg_data;
 	uint16_t i2c_switch_reg_delayMs;
+#endif
 };
 
 /**
