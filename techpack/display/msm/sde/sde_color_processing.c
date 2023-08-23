e34e9da266fba53d09d5442461f639680fb35872
Davide Garberi <dade.garberi@gmail.com>
2023-08-23 22:55:01 +0200

    techpack: display: sde: Disable PCC filters on FOD press
     * This patch allows disabling all color filters completely on FOD press,
       which is needed only when color invert is enabled
     * This is useful because, even if it doesn't look very good visually,
       it allows having properly working FOD when using color invert
     * The base code is mostly taken from OnePlus' PCC disabling,
       with some changes to adapt it to our usage
    
    Change-Id: Ic01e40448fe205b1628cf60175a55b990aa8864d

--- techpack/display/msm/sde/sde_color_processing.c
+++ techpack/display/msm/sde/sde_color_processing.c
@@ -250,6 +250,9 @@ typedef int (*feature_wrapper)(struct sde_hw_dspp *hw_dspp,
 				   struct sde_hw_cp_cfg *hw_cfg,
 				   struct sde_crtc *hw_crtc);
 
+struct drm_msm_pcc save_pcc;
+bool pcc_enabled = false;
+bool skip_pcc = false;
 
 static struct sde_kms *get_kms(struct drm_crtc *crtc)
 {
@@ -1549,6 +1552,66 @@ static int sde_cp_crtc_checkfeature(struct sde_cp_node *prop_node,
 	return ret;
 }
 
+bool sde_is_fod_pressed(struct drm_crtc *crtc)
+{
+	struct sde_crtc_state *cstate = to_sde_crtc_state(crtc->state);
+
+	return !!cstate->fod_dim_layer;
+}
+
+bool sde_cp_crtc_update_pcc(struct drm_crtc *crtc)
+{
+	struct sde_hw_cp_cfg hw_cfg;
+	struct sde_hw_dspp *hw_dspp;
+	struct sde_hw_mixer *hw_lm;
+	struct sde_crtc *sde_crtc = to_sde_crtc(crtc);
+	struct sde_mdss_cfg *catalog = NULL;
+	u32 num_mixers = sde_crtc->num_mixers;
+	bool pcc_skip_mode;
+	int i = 0;
+
+	pcc_skip_mode = sde_is_fod_pressed(crtc);
+	if (skip_pcc == pcc_skip_mode)
+		return false;
+
+	skip_pcc = pcc_skip_mode;
+	memset(&hw_cfg, 0, sizeof(hw_cfg));
+
+	if (!pcc_skip_mode && pcc_enabled) {
+		hw_cfg.payload = &save_pcc;
+		hw_cfg.len = sizeof(save_pcc);
+	}
+
+	hw_cfg.num_of_mixers = sde_crtc->num_mixers;
+	hw_cfg.last_feature = 0;
+
+	for (i = 0; i < num_mixers; i++) {
+		hw_dspp = sde_crtc->mixers[i].hw_dspp;
+		if (!hw_dspp || i >= DSPP_MAX)
+			continue;
+		hw_cfg.dspp[i] = hw_dspp;
+	}
+
+	catalog = get_kms(&sde_crtc->base)->catalog;
+	hw_cfg.broadcast_disabled = catalog->dma_cfg.broadcast_disabled;
+	for (i = 0; i < num_mixers; i++) {
+
+		hw_lm = sde_crtc->mixers[i].hw_lm;
+		hw_dspp = sde_crtc->mixers[i].hw_dspp;
+		if (!hw_lm)
+			continue;
+		if (!hw_dspp || !hw_dspp->ops.setup_pcc)
+			continue;
+
+		hw_cfg.ctl = sde_crtc->mixers[i].hw_ctl;
+		hw_cfg.mixer_info = hw_lm;
+		hw_cfg.displayh = num_mixers * hw_lm->cfg.out_width;
+		hw_cfg.displayv = hw_lm->cfg.out_height;
+		hw_dspp->ops.setup_pcc(hw_dspp, &hw_cfg);
+	}
+	return true;
+}
+
 static void sde_cp_crtc_setfeature(struct sde_cp_node *prop_node,
 				   struct sde_crtc *sde_crtc)
 {
@@ -1585,8 +1648,24 @@ static void sde_cp_crtc_setfeature(struct sde_cp_node *prop_node,
 				cstate->color_invert_on = false;
 				hw_cfg.payload = NULL;
 				hw_cfg.len = 0;
-			} else
+			} else {
 				cstate->color_invert_on = true;
+
+				if (hw_cfg.payload && (hw_cfg.len == sizeof(save_pcc))) {
+					memcpy(&save_pcc, hw_cfg.payload, hw_cfg.len);
+					pcc_enabled = true;
+
+					if (sde_is_fod_pressed(&sde_crtc->base)) {
+						hw_cfg.payload = NULL;
+						hw_cfg.len = 0;
+						skip_pcc = true;
+					} else {
+						skip_pcc = false;
+					}
+				} else {
+					pcc_enabled = false;
+				}
+			}
 		}
 	}
 #endif
@@ -2025,10 +2104,12 @@ void sde_cp_crtc_apply_properties(struct drm_crtc *crtc)
 	struct sde_crtc *sde_crtc = NULL;
 	bool set_dspp_flush = false, set_lm_flush = false;
 	struct sde_cp_node *prop_node = NULL, *n = NULL;
+	struct sde_crtc_state *cstate = to_sde_crtc_state(crtc->state);
 	struct sde_hw_ctl *ctl;
 	u32 num_mixers = 0, i = 0;
 	int rc = 0;
 	bool need_flush = false;
+	bool dirty_pcc = false;
 
 	if (!crtc || !crtc->dev) {
 		DRM_ERROR("invalid crtc %pK dev %pK\n", crtc,
@@ -2050,6 +2131,12 @@ void sde_cp_crtc_apply_properties(struct drm_crtc *crtc)
 
 	mutex_lock(&sde_crtc->crtc_cp_lock);
 	_sde_clear_ltm_merge_mode(sde_crtc);
+
+	if (cstate->color_invert_on) {
+		dirty_pcc = sde_cp_crtc_update_pcc(crtc);
+		if (dirty_pcc)
+			set_dspp_flush = true;
+	}
 
 	if (list_empty(&sde_crtc->dirty_list) &&
 			list_empty(&sde_crtc->ad_dirty) &&
