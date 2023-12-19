c355709295a8d5d810b422ef67e3c9b22ec304b2
Nolen Johnson <johnsonnolen@gmail.com>
2023-12-19 00:58:25 -0500

    drivers: soc: qcom: icnss2: Fix strict-prototypes error
    Change-Id: I18a2b3c8d1c56c0859206eca0177fd3bd1ca9fcc

--- drivers/soc/qcom/icnss2/qmi.c
+++ drivers/soc/qcom/icnss2/qmi.c
@@ -135,7 +135,7 @@ static int num_of_products(moto_product *list)
 	return num;
 }
 
-static int get_moto_device()
+static int get_moto_device(void)
 {
         char *bootdevice = NULL;
         int rc = 0;
@@ -150,7 +150,7 @@ static int get_moto_device()
 }
 
 
-static int get_moto_radio()
+static int get_moto_radio(void)
 {
         char *radiodevice = NULL;
         int rc = 0;
