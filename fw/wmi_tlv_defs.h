eb0e117a36ae7272042deefdd6b82afbdd03996a
spuligil <spuligil@qti.qualcomm.com>
2026-04-29 18:01:54 -0700

    fw-api: CL 32387418 - update fw common interface files
    Change-Id: I5584f2694327b570594a95f74eb54082dd7eada1
    CRs-Fixed: 3830439

--- fw/wmi_tlv_defs.h
+++ fw/wmi_tlv_defs.h
@@ -1623,6 +1623,8 @@ typedef enum {
     WMITLV_TAG_STRUC_wmi_tdls_data_stats,
     WMITLV_TAG_STRUC_wmi_get_chipset_logging_stats_cmd_fixed_param,
     WMITLV_TAG_STRUC_wmi_get_chipset_logging_stats_event_fixed_param,
+    WMITLV_TAG_STRUC_wmi_anomaly_report_hdr,
+    WMITLV_TAG_STRUC_wmi_anomaly_entry_t,
 } WMITLV_TAG_ID;
 /*
  * IMPORTANT: Please add _ALL_ WMI Commands Here.
@@ -2597,6 +2599,7 @@ typedef enum {
     OP(WMI_NAN_DFS_CHANNEL_AVAILABILITY_IND_EVENTID) \
     OP(WMI_TDLS_STATS_EVENTID) \
     OP(WMI_GET_CHIPSET_LOGGING_STATS_EVENTID) \
+    OP(WMI_ANOMALY_REPORT_EVENTID) \
     /* add new EVT_LIST elements above this line */
 
 
@@ -8773,6 +8776,16 @@ WMITLV_CREATE_PARAM_STRUC(WMI_SCAN_CACHE_RESULT_EVENTID);
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_pdev_multi_vdev_ac_queue_depth_event_fixed_param, wmi_pdev_multi_vdev_ac_queue_depth_event_fixed_param, fixed_param, WMITLV_SIZE_FIX) \
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_ARRAY_STRUC, wmi_vdev_ac_info, vdev_ac_info, WMITLV_SIZE_VAR)
 WMITLV_CREATE_PARAM_STRUC(WMI_PDEV_MULTI_VDEV_AC_QUEUE_DEPTH_EVENTID);
+
+/* WMI Anomaly Report Event */
+#define WMITLV_TABLE_WMI_ANOMALY_REPORT_EVENTID(id,op,buf,len) \
+    WMITLV_ELEM(id,op,buf,len, \
+        WMITLV_TAG_STRUC_wmi_anomaly_report_hdr, \
+        wmi_anomaly_report_hdr, hdr, WMITLV_SIZE_FIX) \
+    WMITLV_ELEM(id,op,buf,len, \
+        WMITLV_TAG_ARRAY_STRUC, \
+        wmi_anomaly_entry_t, entries, WMITLV_SIZE_VAR)
+WMITLV_CREATE_PARAM_STRUC(WMI_ANOMALY_REPORT_EVENTID);
 
 #define WMITLV_TABLE_WMI_PDEV_SET_TX_CHAINMASK_COMP_EVENTID(id,op,buf,len) \
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_pdev_set_tx_chainmask_comp_event_fixed_param, wmi_pdev_set_tx_chainmask_comp_event_fixed_param, fixed_param, WMITLV_SIZE_FIX)
